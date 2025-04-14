810e9daae9c160dec3e168e4b43b6c77f60cbcfa
Hassan Hossam <egoker1234@gmail.com>
2025-04-14 13:41:56 +0200

    updating

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import SplashScreen from './components/splash_screen/splash';
-import Cookies from 'js-cookie'; // Import js-cookie
+import Cookies from 'js-cookie';
 import './global.scss';
 import { PreLoading } from './utils/components';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [splash, setSplash] = useState(true);
-  const [imagesLoaded, setImagesLoaded] = useState(false); // Adding a state for image loading
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
   const images = [
-    'https://i.imgur.com/zw0pU6l.jpg',
-    'https://i.imgur.com/jjoLhfT.jpg',
-    'https://i.imgur.com/eOhaVyO.jpg',
-    'https://i.imgur.com/b9J0Pd3.jpg',
     '/icons/s.png',
     '/icons/c.png',
     '/icons/h.png',
@@ -35,49 +31,52 @@ function App() {
           img.onerror = () => reject();
         })
     );
-    Promise.all(promises)
-      .then(() => {
-        setImagesLoaded(true);
-      })
-      .catch((err) => setImagesLoaded(true));
+
+    // Timeout: بعد 3 ثواني نكمل حتى لو الصور ما خلصتش
+    const timeout = new Promise((resolve) => setTimeout(() => resolve(), 3000));
+
+    return Promise.race([
+      Promise.all(promises).then(() => setImagesLoaded(true)),
+      timeout.then(() => setImagesLoaded(true)),
+    ]);
   };
 
   useEffect(() => {
-    checkImagesLoaded();
-
     const visitedBefore = Cookies.get('visitedBefore');
 
-    if (visitedBefore) {
-      setSplash(false);
-    } else {
-      const timer = setTimeout(() => {
+    const loadSplash = async () => {
+      await checkImagesLoaded();
+
+      if (visitedBefore) {
         setSplash(false);
-        Cookies.set('visitedBefore', 'true', { expires: 30 });
-      }, 2500);
-      return () => clearTimeout(timer);
-    }
+      } else {
+        const timer = setTimeout(() => {
+          setSplash(false);
+          Cookies.set('visitedBefore', 'true', { expires: 30 });
+        }, 2500);
+        return () => clearTimeout(timer);
+      }
+    };
+
+    loadSplash();
   }, []);
 
+  // مبنستناش window.load
   useEffect(() => {
-    const handleLoad = () => {
-      setLoading(false);
-    };
-
-    window.addEventListener('load', handleLoad);
-
-    return () => {
-      window.removeEventListener('load', handleLoad);
-    };
+    setLoading(false);
   }, []);
 
+  // مرحلة التحميل
   if (loading || !imagesLoaded) {
     return <PreLoading />;
   }
 
+  // شاشة البداية
   if (splash) {
     return <SplashScreen />;
   }
 
+  // التطبيق الأساسي
   return (
     <Router>
       <Routes>
